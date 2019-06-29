64fe80082d51cb6e7fbeaf11ab79ae1180f532fa
Dru Kelly <drukelly@gmail.com>
2019-06-28 17:56:32 -0700

    insert data into mongodb

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,16 @@
 /* eslint-disable no-unused-expressions */
 const fetch = require('node-fetch')
-const path = require('path')
+const mongojs = require('mongojs')
+
+const databaseURL = process.env.mongo_db
+const collections = [process.env.mongo_collection]
+const mongodb = mongojs(databaseURL, collections)
 
 module.exports = (app) => {
   app.get('/api/get-pets/:type', (req, res) => {
     const formData = new URLSearchParams()
-    formData.append('grant_type', 'client_credentials'),
-    formData.append('client_id', process.env.PETFINDER_ID),
+    formData.append('grant_type', 'client_credentials')
+    formData.append('client_id', process.env.PETFINDER_ID)
     formData.append('client_secret', process.env.PETFINDER_SECRET)
     fetch('https://api.petfinder.com/v2/oauth2/token', {
       body: formData.toString(),
@@ -29,6 +33,16 @@ module.exports = (app) => {
           .then(result => result.json())
           .then(response => {
             pets.push(response)
+            mongodb.on('error', error => console.log('Database Error: ', error))
+            let animals = req.params.type
+            switch (animals) {
+              case 'dog':
+                mongodb.dogs.insert(pets)
+                break
+              case 'cat':
+                mongodb.cats.insert(pets)
+                break
+            }
             res.json(pets)
           })
       })
--- routes/html.js
+++ routes/html.js
@@ -6,4 +6,10 @@ module.exports = app => {
   app.get('/', (_req, res) => {
     res.sendFile(path.join(__dirname, '../public/home.html'))
   })
+  app.get('/dogs', (req, res) => {
+    res.sendFile(path.join(__dirname, `../public/pet.html`))
+  })
+  app.get('/cats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/pet.html'))
+  })
 }
